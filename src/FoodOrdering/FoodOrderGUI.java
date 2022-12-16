11cbc08bc1faba6fa486c8b6881c8a3ae7cf3a28
Jethro <jyty.dev@gmail.com>
2022-12-17 05:14:36 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,10 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FoodOrderGUI extends  JFrame{
     private JPanel panel1;
@@ -34,5 +38,70 @@ public class FoodOrderGUI extends  JFrame{
         foodorder.setDefaultCloseOperation(EXIT_ON_CLOSE);
         foodorder.setVisible(true);
         foodorder.setLocationRelativeTo(null);
+    }
+
+    public void checkOut() {
+        double total = 0;
+        for(JCheckBox item : items) {
+            if(item.isSelected()) {
+                total+=Double.parseDouble(item.getActionCommand());
+            }
+        }
+        total = total-(total*Double.parseDouble(discounts.getSelection().getActionCommand()));
+        JOptionPane.showMessageDialog(panel1,String.format("The total price is Php %.2f", total));
+    }
+
+    public static class OrderEmptyException extends Exception {
+        public OrderEmptyException(String message) {
+            super(message);
+        }
+    }
+
+    ButtonGroup discounts;
+
+    List<JCheckBox> items;
+
+    public FoodOrderGUI() {
+        rbNone.setActionCommand("0");
+        rb5.setActionCommand(".05");
+        rb10.setActionCommand(".1");
+        rb15.setActionCommand(".15");
+        discounts = new ButtonGroup();
+        discounts.add(rbNone);
+        discounts.add(rb5);
+        discounts.add(rb10);
+        discounts.add(rb15);
+
+        cPizza.setActionCommand("100");
+        cBurger.setActionCommand("80");
+        cFries.setActionCommand("65");
+        cSoftDrinks.setActionCommand("55");
+        cTea.setActionCommand("50");
+        cSundae.setActionCommand("45");
+        items = new ArrayList<JCheckBox>();
+        items.add(cPizza);
+        items.add(cBurger);
+        items.add(cFries);
+        items.add(cSoftDrinks);
+        items.add(cTea);
+        items.add(cSundae);
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                checkOut();
+            }
+        });
+    }
+
+    public static class DiscountEmptyException extends  Exception {
+        public DiscountEmptyException(String message) {
+            super(message);
+        }
+    }
+
+    public static class OrderDiscountEmptyException extends Exception {
+        public OrderDiscountEmptyException(String message) {
+            super(message);
+        }
     }
 }
