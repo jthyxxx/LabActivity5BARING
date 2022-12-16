e62b1ff24dcf29a886039ff70726a7f3baf404c6
Jethro <jyty.dev@gmail.com>
2022-12-17 05:19:19 +0800

    Rename SimpleCalc components

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -4,7 +4,13 @@ import javax.swing.*;
 
 public class SimpleCalcGUI {
     private JPanel panel1;
-    private JTextField textField1;
-    private JComboBox comboBox1;
-    private JButton computeResultButton;
+    private JTextField tfNumber1;
+
+    private  JTextField tfNumber2;
+
+    private JComboBox cbOperations;
+
+    private JTextField lblResult;
+
+    private JButton btnCompute;
 }
